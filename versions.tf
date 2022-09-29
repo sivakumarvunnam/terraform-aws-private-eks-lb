d617fc34d0ab295ad8f4f2b5c4e1e42b9d9cc6b8
Sivakumar Vunnam <sivakumarvunnam1@gmail.com>
2022-09-29 10:15:11 -0500

    created versions.tf file specific version of provider

---
+++ versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.25.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.11.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.4.1"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+  }
+}
